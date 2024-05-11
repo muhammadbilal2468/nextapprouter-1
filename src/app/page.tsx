a93f47e0df4df56c58335f01742128d14e39e929
Muhammad Bilal <muhammadbilalll2468@gmail.com>
2024-05-11 12:40:34 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_API_URL}`),
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
+  ),
   title: "Home",
   description: "Aplikasi untuk belajar next JS",
   authors: [{ name: "Muhammad Bilal" }],
